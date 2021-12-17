3e9854db43d29fb16a5e1bc1812ceb07b6bc3203
Rodion Stashinsky <vivagelive@gmail.com>
2021-12-17 16:06:57 +0300

    Initial commit

---
+++ src/test/java/com/example/answersboxapi/AnswersBoxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.answersboxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnswersBoxApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
